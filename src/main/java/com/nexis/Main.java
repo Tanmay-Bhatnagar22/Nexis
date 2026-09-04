38c2da996b4814f5d2a057f60abe82ad804a4294
Tanmay Bhatnagar <tanmay.24bcy10155@vitbhopal.ac.in>
2026-09-04 15:40:37 +0530

    Refactor main method to execute NexisCLI using picocli

--- src/main/java/com/nexis/Main.java
+++ src/main/java/com/nexis/Main.java
@@ -1,10 +1,13 @@
 package com.nexis;
 
+import com.nexis.cli.NexisCLI;
+
+import picocli.CommandLine;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("NEXIS");
-        System.out.println("File Integrity & Host Monitor");
-        System.out.println("Nexis v1.0.0");
+        int exitCode = new CommandLine(new NexisCLI()).execute(args);
+        System.exit(exitCode);
     }
 }
