165170b49df28e89b3a15349afd20551d35f40ab
Tanmay Bhatnagar <tanmay.24bcy10155@vitbhopal.ac.in>
2026-09-04 15:09:27 +0530

    Add initial project structure with Maven configuration and main class

---
+++ src/main/java/com/nexis/Main.java
@@ -0,0 +1,10 @@
+package com.nexis;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("NEXIS");
+        System.out.println("File Integrity & Host Monitor");
+        System.out.println("Nexis v1.0.0");
+    }
+}
